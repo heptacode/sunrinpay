f102968deb38e118eb843ab126387e9d9519bf0f
heptacode <admin@hyunwoo.kim>
2020-05-29 19:36:09 +0900

    Register OAuth

--- src/views/user/Auth.vue
+++ src/views/user/Auth.vue
@@ -1,12 +1,57 @@
-<template> </template>
+<template>
+	<div>
+		<div id="firebaseui-auth-container"></div>
+	</div>
+</template>
 
 <script lang="ts">
+import * as firebase from "firebase";
+import * as firebaseui from "firebaseui";
+
 import { Vue, Component } from "vue-property-decorator";
 
-export default class Auth extends Vue {
-	email: string = "admin@hyunwoo.kim";
-	password: string = "test1234";
-}
+import credentials from "@/../credentials";
+
+firebase.initializeApp(credentials.firebaseConfig);
+firebase.analytics();
+firebase.auth().languageCode = "ko";
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+const uiConfig = {
+	callbacks: {
+		signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+			// User successfully signed in.
+			// Return type determines whether we continue the redirect automatically
+			// or whether we leave that to developer to handle.
+			return true;
+		},
+	},
+	// signInFlow: "popup",
+	signInSuccessUrl: "/",
+	signInOptions: [
+		firebase.auth.EmailAuthProvider.PROVIDER_ID,
+		{
+			provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+			defaultCountry: "KR",
+		},
+		firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+		firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+		firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+		firebase.auth.GithubAuthProvider.PROVIDER_ID,
+		"apple.com",
+		"microsoft.com",
+	],
+	// Terms of service url.
+	// tosUrl: "<your-tos-url>",
+	// Privacy policy url.
+	// privacyPolicyUrl: "<your-privacy-policy-url>",
+};
+
+ui.start("#firebaseui-auth-container", uiConfig);
+
+export default class Auth extends Vue {}
 </script>
 
-<style></style>
+<style>
+@import url("https://www.gstatic.com/firebasejs/ui/4.5.0/firebase-ui-auth.css");
+</style>
