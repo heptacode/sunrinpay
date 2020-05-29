c55b770449f1f2de3fbdbcda3bd658739adecf23
Andy0414 <pjh8667@naver.com>
2020-05-29 11:05:32 +0900

    Update pos plus

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,13 @@ Vue.config.productionTip = false;
 
 declare global {
 	interface Number {
-		numberFormat: () => string;
+		numberFormat: (this: number) => string;
 	}
 }
 Number.prototype.numberFormat = function(this: number): string {
 	return new Intl.NumberFormat().format(this);
 };
+Number(19).numberFormat();
 new Vue({
 	router,
 	store,
