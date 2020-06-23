b8f9a170dff70fb800fdcb165174dba70adb65f7
heptacode <admin@hyunwoo.kim>
2020-06-24 00:00:22 +0900

    Update Auth

--- src/views/user/Auth.vue
+++ src/views/user/Auth.vue
@@ -1,8 +1,15 @@
 <template>
 	<div>
-		<div id="firebaseui-auth-container"></div>
-		<button @click="test">test</button>
-		<button @click="signOut">Sign out</button>
+		<div :class="{ inactive: ifAuth }" id="firebaseui-auth-container"></div>
+		<div v-if="ifAuth">
+			<img :src="photoURL" width="32px" height="32px" style="border-radius: 50%" />
+			<h1>{{ displayName }}</h1>
+			<h3>
+				{{ email }} <span>{{ emailVerified ? "인증됨" : "미인증" }}</span>
+			</h3>
+			{{ isAnonymous }}
+			<button @click="signOut">Sign out</button>
+		</div>
 	</div>
 </template>
 
@@ -14,14 +21,17 @@ import { Vue, Component } from "vue-property-decorator";
 
 import { db } from "@/DB";
 
-@Component({
-	firestore: {
-		documents: db.collection("accounts"),
-	},
-})
+@Component({})
 export default class Auth extends Vue {
-	documents: any[] = [];
+	ifAuth: boolean = false;
 	uid: string = "";
+
+	displayName: string = "";
+	email: string = "";
+	emailVerified: boolean = false;
+	photoURL: string = "";
+	isAnonymous: boolean = false;
+
 	idToken: string = "";
 
 	mounted() {
@@ -29,9 +39,7 @@ export default class Auth extends Vue {
 		const uiConfig = {
 			callbacks: {
 				signInSuccessWithAuthResult: (authResult, redirectUrl) => {
-					// User successfully signed in.
-					// Return type determines whether we continue the redirect automatically
-					// or whether we leave that to developer to handle.
+					//
 					return true;
 				},
 			},
@@ -52,40 +60,44 @@ export default class Auth extends Vue {
 				"apple.com",
 				"microsoft.com",
 			],
-			// Terms of service url.
-			// tosUrl: "<your-tos-url>",
-			// Privacy policy url.
+			tosUrl: "https://sunrinpay.web.app/privacy",
 			privacyPolicyUrl: "https://sunrinpay.web.app/privacy",
 		};
 
-		ui.start("#firebaseui-auth-container", uiConfig);
-
 		firebase.auth().onAuthStateChanged(user => {
 			if (user) {
-				console.log("LOGIN : %O", user);
-				// name = user.displayName;
-				// email = user.email;
-				// photoUrl = user.photoURL;
-				// emailVerified = user.emailVerified;
+				this.ifAuth = true;
 				this.uid = user.uid;
-				user.getIdToken().then(idToken => console.log("TOKEN : " + idToken));
 
-				console.log("uid : " + this.uid);
-				let userData = {
-					displayName: user.displayName,
-					email: user.email,
-					emailVerified: user.emailVerified,
-					photoURL: user.photoURL,
-					isAnonymous: user.isAnonymous,
-					uid: user.uid,
-					providerData: user.providerData,
-				};
-				db.collection("accounts")
-					.doc(user.uid)
-					.set(userData)
-					.then(() => console.log("User updated!"))
-					.catch(err => console.log("Error : " + err));
+				this.displayName = user.displayName!;
+				this.email = user.email!;
+				this.emailVerified = user.emailVerified;
+				this.photoURL = user.photoURL!;
+				this.isAnonymous = user.isAnonymous;
+
+				console.log("LOGIN");
+
+				(async () => {
+					await user.getIdToken().then(idToken => (this.idToken = idToken));
+					let userData = {
+						displayName: user.displayName,
+						email: user.email,
+						emailVerified: user.emailVerified,
+						photoURL: user.photoURL,
+						isAnonymous: user.isAnonymous,
+						uid: user.uid,
+						providerData: user.providerData,
+						idToken: this.idToken,
+					};
+					await db
+						.collection("accounts")
+						.doc(user.uid)
+						.set(userData)
+						.then(() => console.log("User updated!"))
+						.catch(err => console.log("Error : " + err));
+				})();
 			} else {
+				ui.start("#firebaseui-auth-container", uiConfig);
 				console.log("Not Signed in");
 			}
 		});
@@ -94,15 +106,20 @@ export default class Auth extends Vue {
 		firebase
 			.auth()
 			.signOut()
-			.then(() => console.log("Sign out successful."))
+			.then(() => {
+				this.ifAuth = false;
+				this.emailVerified = this.isAnonymous = false;
+				this.uid = this.displayName = this.email = this.photoURL = this.idToken = "";
+				console.log("Sign out successful.");
+			})
 			.catch(err => console.log("Error : " + err));
-	}
-	test() {
-		console.log("docs : " + JSON.stringify(this.documents));
 	}
 }
 </script>
 
 <style>
 @import url("https://www.gstatic.com/firebasejs/ui/4.5.0/firebase-ui-auth.css");
+.inactive {
+	display: none;
+}
 </style>
