8fa028a9689b22084a5f5b4755ea60a4a22d9a72
heptacode <admin@hyunwoo.kim>
2020-06-26 09:59:39 +0900

    Update schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,7 +1,7 @@
 export interface StockItem {
 	name: string; // 상품의 이름
 	barcode: string; // 바코드 문자열
-	count: number; // 재고
+	quantity: number; // 재고
 	price: number; // 가격
-	tag?: string[]; // 상품 태그
+	tags?: string[]; // 상품 태그
 }
