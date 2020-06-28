a063e586c164a4629dcca57b2df7db532bb36c29
heptacode <admin@hyunwoo.kim>
2020-06-29 00:26:54 +0900

    Update Credentials

--- src/DB.ts
+++ src/DB.ts
@@ -1,9 +1,18 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 
-import credentials from "@/../credentials";
-
-export const db = firebase.initializeApp(credentials.firebaseConfig).firestore();
+export const db = firebase
+	.initializeApp({
+		apiKey: process.env.FIREBASE_API_KEY || "AIzaSyC4TcH94TCyNYppi8VadESHta-6u6VTPts",
+		authDomain: "sunrinpay.firebaseapp.com",
+		databaseURL: "https://sunrinpay.firebaseio.com",
+		projectId: "sunrinpay",
+		storageBucket: "sunrinpay.appspot.com",
+		messagingSenderId: "604565159530",
+		appId: "1:604565159530:web:f5a102d54869c502051413",
+		measurementId: "G-1ZF3X6NF8V",
+	})
+	.firestore();
 firebase.auth().languageCode = "ko";
 
 // Export types that exists in Firestore
