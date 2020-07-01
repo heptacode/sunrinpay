6c8c8a54da4bd6c26e0e14bdcfa06db4983241df
Andy0414 <pjh8667@naver.com>
2020-07-01 13:28:24 +0900

    Update qrscanner

---
+++ src/components/QRScanner.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="qrscanner">
+		<qrcode-stream @decode="onDecode"></qrcode-stream>
+	</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Watch } from "vue-property-decorator";
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from "vue-qrcode-reader";
+@Component({
+	components: {
+		QrcodeStream
+	}
+})
+export default class QRScanner extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+</style>
