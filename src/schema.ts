e39689108d28d0239f7cf16369d0641fd8b2530f
heptacode <admin@hyunwoo.kim>
2020-07-06 08:36:26 +0900

    Update schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,8 +1,8 @@
 export interface StockItem {
+	id?: string; // Firestore 도큐멘트 ID
 	name: string; // 상품의 이름
 	barcode: string; // 바코드 문자열
 	quantity: number; // 재고
 	price: number; // 가격
-	id?: string;
 	tags?: string[]; // 상품 태그
 }
