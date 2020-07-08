4e0f639e17f4ece8eda43c86817c7edc07cf378c
Andy0414 <pjh8667@naver.com>
2020-07-08 13:53:04 +0900

    Update sendMoneyDesign

--- src/views/SendMoney.vue
+++ src/views/SendMoney.vue
@@ -5,21 +5,36 @@
 			<input type="tel" v-model="amount" minlength="1" maxlength="6" required />
 			<NumberCounter :text="amount.toString()" :isNumberFormat="true" defaultChar="0"></NumberCounter>원
 			<div class="sendmoney__bank">
-				<div>
-					<span @click="bank = 'NH농협'"><i class="iconify" data-icon="mdi-bank"></i> NH 농협1</span>
-					<span @click="bank = '신한은행'"><i class="iconify" data-icon="mdi-bank"></i> 신한은행</span>
-					<span><i class="iconify" data-icon="mdi-bank"></i> NH 농협3</span>
-				</div>
-				<div>
-					<span><i class="iconify" data-icon="mdi-bank"></i> NH 농협4</span>
-					<span><i class="iconify" data-icon="mdi-bank"></i> NH 농협5</span>
-					<span><i class="iconify" data-icon="mdi-bank"></i> NH 농협6</span>
-				</div>
-				<div>
-					<span><i class="iconify" data-icon="mdi-bank"></i> NH 농협7</span>
-					<span><i class="iconify" data-icon="mdi-bank"></i> NH 농협8</span>
-					<span><i class="iconify" data-icon="mdi-bank"></i> NH 농협9</span>
-				</div>
+				<span @click="bank = 'NH농협'">
+					<i class="iconify" data-icon="mdi-bank"></i> NH 농협1
+				</span>
+				<span @click="bank = '신한은행'">
+					<i class="iconify" data-icon="mdi-bank"></i> 신한은행
+				</span>
+				<span>
+					<i class="iconify" data-icon="mdi-bank"></i> NH 농협3
+				</span>
+				<span>
+					<i class="iconify" data-icon="mdi-bank"></i> NH 농협4
+				</span>
+				<span>
+					<i class="iconify" data-icon="mdi-bank"></i> NH 농협5
+				</span>
+				<span>
+					<i class="iconify" data-icon="mdi-bank"></i> NH 농협6
+				</span>
+				<span>
+					<i class="iconify" data-icon="mdi-bank"></i> NH 농협7
+				</span>
+				<span>
+					<i class="iconify" data-icon="mdi-bank"></i> NH 농협8
+				</span>
+				<span>
+					<i class="iconify" data-icon="mdi-bank"></i> NH 농협9
+				</span>
+				<span>
+					<i class="iconify" data-icon="mdi-bank"></i> NH 농협10
+				</span>
 			</div>
 			<input type="text" v-model="accountNo" placeholder="계좌번호" minlength="6" required />
 			<button type="submit">QR 생성</button>
@@ -38,8 +53,8 @@ import { Vue, Component } from "vue-property-decorator";
 @Component({
 	components: {
 		QRcode,
-		NumberCounter: NumberCounterVue,
-	},
+		NumberCounter: NumberCounterVue
+	}
 })
 export default class SendMoney extends Vue {
 	amount: number = 0;
@@ -59,14 +74,31 @@ export default class SendMoney extends Vue {
 	form {
 		width: 100%;
 		.sendmoney__bank {
-			display: flex;
-			flex-direction: column;
-			background-color: #1f2026;
+			position: fixed;
+			bottom: 0;
 			width: 100%;
-			// justify-content: space-between;
-			div {
-				justify-content: space-between;
+			height: 80%;
+
+			padding: 25px;
+			border-radius: 25px 25px 0 0;
+
+			display: flex;
+			flex-wrap: wrap;
+			background-color: $content-color;
+			span {
+				flex: 1 1 30%;
+				margin: 20px;
+				border-radius: 20px;
+				background-color: $background-color;
+
+				display: flex;
+				justify-content: center;
 				align-items: center;
+				flex-direction: column;
+
+				.iconify {
+					font-size: 2em;
+				}
 			}
 		}
 	}
