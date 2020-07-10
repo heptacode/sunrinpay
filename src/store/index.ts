fd1819b53998c4339570b0dd36322306c7c9c8a0
heptacode <admin@hyunwoo.kim>
2020-07-10 12:08:01 +0900

    Add SEND_MONEY

--- src/store/index.ts
+++ src/store/index.ts
@@ -7,6 +7,7 @@ import axios from "axios";
 import { db, log, transaction } from "@/DB";
 
 const event = require("vue-analytics").event;
+const docRef = db.collection("accounts").doc(firebase.auth().currentUser!.uid);
 
 Vue.use(Vuex);
 
@@ -16,21 +17,61 @@ export default new Vuex.Store({
 	},
 	mutations: {},
 	actions: {
-		async GET_BALANCE({ commit, state }, data) {
+		async GET_BALANCE({ commit, state }, data): Promise<boolean | number> {
 			event("action", "GET_BALANCE", "getBalance", data);
 			try {
-				let doc = await db
-					.collection("accounts")
-					.doc(firebase.auth().currentUser!.uid)
-					.get();
-				state.balance = doc.data()!.balance;
+				let snapshot = await docRef.get();
+				state.balance = snapshot.data()!.balance;
 				return state.balance;
 			} catch (err) {
 				log("error", `CREATE_ORDER : ${err}`);
 				return false;
 			}
 		},
-		async CREATE_ORDER({ commit, state }, data) {
+		async SEND_MONEY({ commit, state }, data): Promise<boolean | string> {
+			event("action", "SEND_MONEY", "sendMoney", data);
+			try {
+				// 보내는 사람 도큐먼트 가져오기
+				let snapshot = await docRef.get();
+
+				// state 잔액 갱신
+				state.balance = snapshot.data()!.balance;
+				if (state.balance - data.amount > 0) return `잔액이 ${Math.abs(state.balance - data.amount)}원 부족합니다.`;
+
+				// 보내는 사람 잔액 차감
+				await docRef.update({
+					balance: state.balance - data.amount,
+				});
+
+				// 받는 사람 도큐멘트의 uid 조회
+				let query = await db
+					.collection("accounts")
+					.where("email", "==", data.recipient)
+					.get();
+
+				// 받는 사람  도큐먼트 가져오기
+				let recipientDocRef = await db.collection("accounts").doc(query[0].doc.id);
+				let recipientSnapshot = await recipientDocRef.get();
+
+				// 받는 사람 잔액 조회
+				recipientSnapshot.data()!.balance;
+
+				// 받는 사람 잔액 추가
+				await recipientDocRef.update({
+					balance: recipientSnapshot.data()!.balance + data.amount,
+				});
+
+				// state 잔액 갱신
+				snapshot = await docRef.get();
+				state.balance = snapshot.data()!.balance;
+
+				return true;
+			} catch (err) {
+				log("error", `SEND_MONEY : ${err}`);
+				return false;
+			}
+		},
+		async CREATE_ORDER({ commit, state }, data): Promise<boolean> {
 			event("action", "CREATE_ORDER", "createOrder", data);
 			try {
 				await db
@@ -43,16 +84,16 @@ export default new Vuex.Store({
 				return false;
 			}
 		},
-		async CHECKOUT({ commit, state }, data) {
+		async CHECKOUT({ commit, state }, data): Promise<boolean | string> {
 			event("action", "CHECKOUT", "checkout", data);
-			let doc = db.collection("accounts").doc(firebase.auth().currentUser!.uid);
-			let snapshot = await doc.get();
+			let snapshot = await docRef.get();
 			const newBalance = snapshot.data()!.balance - data.price;
 			if (newBalance >= 0) {
 				// 결제 가능
 				try {
-					await doc.update({ balance: newBalance }).then(() => transaction({ price: data.price }));
-					return newBalance;
+					await docRef.update({ balance: newBalance });
+					await transaction({ price: data.price });
+					return true;
 				} catch (err) {
 					log("error", `결제 후 잔고 업데이트 실패 : ${err}`);
 					return false;
