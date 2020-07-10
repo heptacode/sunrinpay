3dca6a6c42c60928133206db0e1f4b432e862eca
Andy0414 <pjh8667@naver.com>
2020-07-10 10:34:25 +0900

    Update checkout kakaopay

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -30,7 +30,11 @@
 			</ul>
 		</div>
 		<div class="detailsmode__actions">
-			<PaymentButton class="detailsmode__actions__btn"></PaymentButton>
+			<PaymentButton
+				class="detailsmode__actions__btn"
+				paymentName="Kakao Pay"
+				@click="checkoutWithKakaoPay"
+			></PaymentButton>
 			<PaymentButton class="detailsmode__actions__btn"></PaymentButton>
 		</div>
 	</div>
@@ -42,6 +46,9 @@ import CashButtonVue from "@/components/CashButton.vue";
 import NumberCounterVue from "vue-roller";
 
 import { Vue, Component } from "vue-property-decorator";
+
+import isMobile from "@/lib/isMobile";
+
 @Component({
 	components: {
 		PaymentButton: PaymentButtonVue,
@@ -59,8 +66,11 @@ export default class Checkout extends Vue {
 			tax_free_amount: 0,
 			price: 1000
 		});
-		console.log(res);
-		// window.open(res.next_redirect_pc_url);
+		if (isMobile()) {
+			window.open(res.next_redirect_mobile_url);
+		} else {
+			window.open(res.next_redirect_pc_url);
+		}
 	}
 }
 </script>
