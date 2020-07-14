5cfa2f04cf654005dff6491f9e617c38a8a586d0
Andy0414 <pjh8667@naver.com>
2020-07-15 08:41:33 +0900

    Update service worker

---
+++ docs/precache-manifest.fb9f27562dde1bbb0a9b91bd1a051c7e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93d20e73a69212ca6d66889aec21db24",
+    "url": "/.well-known/microsoft-identity-association.json"
+  },
+  {
+    "revision": "c2c4242c4b68b3ed2f0c3f2fffa06518",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "b3b9d98c539f11b05ac1",
+    "url": "/css/app.eec901ed.css"
+  },
+  {
+    "revision": "774860106177b0bf59b7",
+    "url": "/css/chunk-vendors.5e44303a.css"
+  },
+  {
+    "revision": "7ecee500db8a78044580a9b305e826a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3b9d98c539f11b05ac1",
+    "url": "/js/app.3fb39976.js"
+  },
+  {
+    "revision": "774860106177b0bf59b7",
+    "url": "/js/chunk-vendors.780ac621.js"
+  },
+  {
+    "revision": "ac74842d3a48a64c703eb8534cfbc64d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "ce99b9b49010036773a7b96baaeed34a",
+    "url": "/ms-icon/ms-icon-150x150.png"
+  },
+  {
+    "revision": "441ac08410c6ee9e9d7dbbf01e67e86d",
+    "url": "/ms-icon/ms-icon-310x310.png"
+  },
+  {
+    "revision": "5701becb334fe559aaf262a16731aeff",
+    "url": "/ms-icon/ms-icon-70x70.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "17ecfaee522eaf5b3ad9c9aa1b2973cc",
+    "url": "/service-worker.js"
+  }
+]);
---
+++ docs/service-worker.js
@@ -0,0 +1,8 @@
+importScripts("/precache-manifest.fb9f27562dde1bbb0a9b91bd1a051c7e.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+if (workbox) {
+    console.log(`Workbox is loaded`);
+}
+else {
+    console.log(`Workbox didn't load`);
+}
